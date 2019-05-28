911e37d87a078f1fe33eac7c04417eba3b2c43e9
rhpamAdmin <>
2019-05-28 08:58:53 -0400

    {/src/main/java/com/RulesFired.java}

--- src/main/java/com/RulesFired.java
+++ src/main/java/com/RulesFired.java
@@ -72,7 +72,7 @@ public class RulesFired implements java.io.Serializable {
 		this.eventResponsePayload = eventResponsePayload;
 	}
 	
-    @Override
+    
     public boolean equals(Object obj) 
     { 
           
@@ -83,7 +83,7 @@ public class RulesFired implements java.io.Serializable {
   
   
       
-    @Override
+   
     public int hashCode() 
     { 
           
