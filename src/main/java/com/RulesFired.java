bdf4cc7ce24fd1a37dcf8e092cbe3292e01e364c
rhpamAdmin <>
2019-05-28 09:05:32 -0400

    {/src/main/java/com/RulesFired.java}

--- src/main/java/com/RulesFired.java
+++ src/main/java/com/RulesFired.java
@@ -76,11 +76,7 @@ public class RulesFired implements java.io.Serializable {
     public boolean equals(Object obj) 
     { 
     com.RulesFired rulesFired = (com.RulesFired)obj;      
-   if(getEventCategory().equals(rulesFired.getEventCategory()))) {
-            return true; 
-    }else {
-        return false;
-    }
+    return Objects.equals(eventCategory,rulesFired.getEventCategory());
    
           
     
